2cc2d5769abc6b7ff40e9027d5e1ee369ce94c8e
InHuman66 <olexandr.kk@gmail.com>
2020-08-24 18:26:21 +0300

    Updates

---
+++ precache-manifest.1106e59811e8bbab59ed4fd98b64a976.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599a6320d2fb1cb147962ec25681615f",
+    "url": "/Home-Work/index.html"
+  },
+  {
+    "revision": "659b3e3c3b457b406bd3",
+    "url": "/Home-Work/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e6f13d807f369d1855be",
+    "url": "/Home-Work/static/css/main.5458f664.chunk.css"
+  },
+  {
+    "revision": "659b3e3c3b457b406bd3",
+    "url": "/Home-Work/static/js/2.f4394c6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Home-Work/static/js/2.f4394c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f13d807f369d1855be",
+    "url": "/Home-Work/static/js/main.c624cb87.chunk.js"
+  },
+  {
+    "revision": "d81b9da4e10dd58c71b7",
+    "url": "/Home-Work/static/js/runtime-main.6749586f.js"
+  },
+  {
+    "revision": "1ee966d656f02e2e41912d7622009854",
+    "url": "/Home-Work/static/media/avatar7.1ee966d6.png"
+  }
+]);
